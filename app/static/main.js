a8d93a210e7283ccadca94b941f1d8f733fa52f1
leriahes <valeriya-shevchenko-2002@mail.ru>
2023-09-14 01:04:21 +0700

    Ввод значений

--- app/static/main.js
+++ app/static/main.js
@@ -6,24 +6,119 @@ else {
     console.log(123);
 }
 
+values = [];
+
 $(document).ready(function(){
     $('#drawGraph').click(function(e)
     {
         e.preventDefault();
+
+        values = [];
+        let cells = document.querySelectorAll('.form-control')
+
+        for(let i = 0; i < cells.length; i++)
+        {
+            values.push({val : cells[i].value});
+        }
+
         $.ajax({
             url: '',
             type: 'get',
-            contenType: 'application/json',
+            contentType: 'application/json',
             data: {
                 button_text: $(this).text(),
+                cells_values: JSON.stringify(values),
             },
-            /*success: function(){ // если запрос успешен вызываем функцию
-                $('#drawGraph').style.visible(); // добавляем текст в элемент с классом .myClass
-            },*/
             success: function(response){
-               // $('#drawGraph').text(response.secs + 123);
-                let a = document.querySelector('#cardChart1');
-                a.style.display = 'block';
+                $('#drawGraph').text(response.secs + 123);
+                /*let cells = document.querySelectorAll('.form-control')
+                //$('#drawGraph').text(cells.length + 1);
+                let k = 0;
+                for(let i = 0; i < cells.length; i++)
+                {
+                    let count = 0;
+
+                    for(let j = 0; j < cells[i].value.length; j++)
+                    {
+                        if (cells[i].value[j] >= '0' && cells[i].value[j] <= '9')
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    //$('#findDecision').text(count);
+
+                    if (count < cells[i].value.length || cells[i].value.length == 0 || cells[i].value.length == 1 && cells[i].value[0] == '0')
+                    {
+                        cells[i].style.backgroundColor = "#ea9999";
+                    }
+                    else
+                    {
+                        cells[i].style.backgroundColor = "#ffffff";
+                        k++;
+                    }
+
+                }
+
+                //$('#drawGraph').text(k);
+                */
+                
+                if (response.bad.length != 0)
+                {
+                    bad_val = response.bad.split(' ');
+                    
+                    j = 0;
+                    for (i = 0; i < cells.length; i++)
+                    {
+                        k = parseInt(bad_val[j]);
+
+                        if (k == i)
+                        {
+                            cells[i].style.backgroundColor = "#ea9999";
+                            j++;
+                        }
+                        else
+                        {
+                            cells[i].style.backgroundColor = "#ffffff";
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    for (i = 0; i < cells.length; i++)
+                    {
+                        cells[i].style.backgroundColor = "#ffffff";
+                    }
+
+                    let a = document.querySelector('#cardChart1');
+                    a.style.display = 'block';
+                }
+                /*if (k < cells.length)
+                {
+                    //$('#drawGraph').text('Всё плохо');
+                }
+                else
+                {
+                    //$('#drawGraph').text(k);
+                    /*if (cells[0].value == '1')
+                    {
+                        $('#findDecision').text(45);
+                    }
+                    else
+                    {
+                        $('#findDecision').text(cells[0].value);
+                    }
+                    //$('#drawGraph').text(k);*/
+
+                   /* let a = document.querySelector('#cardChart1');
+                    a.style.display = 'block';
+                }*/
+                
 
 
             }
--- app/views.py
+++ app/views.py
@@ -11,13 +11,51 @@ import plotly.express as px
 @app.route('/nx2')
 def index():
 
-    secs = 345
     if request.is_json:
         secs = time()
-        #request.args.get('button_text')
-        return jsonify({'secs': secs})
-    
+        print('yaas')
 
+        cells = request.args.get('cells_values')
+
+        i = 0
+
+        cells_values = cells.split( '"},{"val":"')
+        cells_values[0] = cells_values[0][9:len(cells_values[0])]
+        cells_values[len(cells_values) - 1] = cells_values[len(cells_values) - 1][0:len(cells_values[len(cells_values) - 1]) - 3]
+        print(cells_values)
+
+        start_values = []
+        bad_values = []
+
+
+        while i < len(cells_values):
+            count = 0
+
+            for j in range(len(cells_values[i])):
+
+                if cells_values[i][j] >= '0' and cells_values[i][j] <= '9':
+                    count += 1
+                else:
+                    break
+
+            if count < len(cells_values[i]) or len(cells_values[i]) == 0 or len(cells_values[i]) == 1 and cells_values[i][0] == '0':
+                bad_values.append(str(i))
+                    #cells[i].style.backgroundColor = "#ea9999";
+            else:
+                    #cells[i].style.backgroundColor = "#ffffff";
+                start_values.append(int(cells_values[i]))
+
+            i += 1
+        
+        print(start_values)
+        print(bad_values)
+        print((' ').join(bad_values))
+        #request.args.get('button_text')
+        return jsonify({'secs': secs, 'bad': (' ').join(bad_values)})
+    else:
+        print(request.args.get('button_text'))
+
+    print(request.args.get('button_text'))
     # Graph One
     df = pd.DataFrame([
     dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28'),
